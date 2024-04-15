b14a96f995c224a11f568a7ab4f330486aeea929
toma <fko2347021@stu.o-hara.ac.jp>
2024-04-15 10:31:24 +0900

    nanka

---
+++ src/main/java/com/example/Ateam/AteamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ateam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ateam/AteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ateam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
